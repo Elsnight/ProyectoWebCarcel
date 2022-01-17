ebd9279fec0872966d69bc3875afd3eaa4476398
ByronTosh <by_tosh20@hotmail.com>
2022-01-17 16:49:21 -0500

    Sprint0-Sprint1

---
+++ app/Models/User.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class User extends Authenticatable implements MustVerifyEmail
+
+
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'role_id',
+        'first_name',
+        'last_name',
+        'username',
+        'personal_phone',
+        'home_phone',
+        'address',
+        'password',
+        'email',
+        'birthdate',
+    ];
+
+
+
+
+    // Relación de uno a muchos
+    // Un usuario le pertenece un rol
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
+    }
+
+    // Relación de uno a muchos
+    // Un usuario puede realizar muchos reportes
+    public function reports()
+    {
+        return $this->hasMany(Report::class);
+    }
+
+
+    // Relación de muchos a muchos
+    // Un usuario puede estar en varios pabellones
+    public function wards()
+    {
+        return $this->belongsToMany(Ward::class)->withTimestamps();
+    }
+
+
+
+    // Relación de muchos a muchos
+    // Un usuario puede estar en varias cárceles
+    public function jails()
+    {
+        return $this->belongsToMany(Jail::class)->withTimestamps();
+    }
+
+
+
+    // Relación polimórfica uno a uno
+    // Un usuario pueden tener una imagen
+    public function image()
+    {
+        return $this->morphOne(Image::class,'imageable');
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+}
---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,20 @@
+<div class="container">
+
+    Bienvenido - {{ Auth::user()->first_name }}
+
+
+    <div>
+        <form action="{{route('logout')}}" method="POST">
+            @csrf
+            <div >
+                <button type="submit">LOGOUT</button>
+            </div>
+        </form>
+    </div>
+
+
+
+
+</div>
+
+
