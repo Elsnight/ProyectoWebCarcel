e7682277311f85d1bd97d2bbc1ac5087512ebaaf
ByronTosh <by_tosh20@hotmail.com>
2022-01-24 16:26:09 -0500

    Dashboard-Actualizar-Perfil

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class Image extends Model
 {
-    
+
     protected $fillable = [
         'path',
     ];
@@ -20,6 +22,16 @@ class Image extends Model
     {
         return $this->morphTo();
     }
+
+    //Esta función permite obtener la url de la imagen
+    public function getUrl()
+    {
+        return Str::startsWith($this->path, 'https://')
+            ? $this->path
+            : Storage::url($this->path);
+    }
+
+
 
 
     use HasFactory;
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use App\Traits\HasImage;
+use Carbon\Carbon;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -12,7 +14,7 @@ class User extends Authenticatable implements MustVerifyEmail
 
 
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, HasImage;
 
     /**
      * The attributes that are mass assignable.
@@ -74,6 +76,21 @@ class User extends Authenticatable implements MustVerifyEmail
     {
         return $this->morphOne(Image::class,'imageable');
     }
+
+
+    public function getFullName()
+    {
+        return "$this->first_name $this->last_name";
+    }
+
+    // Función para mejorar el formato de la fecha de nacimiento
+    public function getBirthdateAttribute($value)
+    {
+        // https://laravel.com/docs/8.x/eloquent-mutators#accessors-and-mutators
+        return isset($value) ? Carbon::parse($value)->format('d/m/Y') : null;
+    }
+
+
 
 
 
